97c26ef2eb8da6b1b5886c031a9c0b6134ceaeca
tamas <tamas@localhost.localdomain>
2020-04-17 12:11:31 +0200

    tmux update

---
+++ hello/target/debug/deps/hello-8b670e104ac7a356.d
@@ -0,0 +1,5 @@
+/home/tamas/repos/tmux/hello/target/debug/deps/hello-8b670e104ac7a356.rmeta: src/main.rs
+
+/home/tamas/repos/tmux/hello/target/debug/deps/hello-8b670e104ac7a356.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/rls/debug/deps/hello-2ce71755fe43920d.d
@@ -0,0 +1,5 @@
+/home/tamas/repos/tmux/hello/target/rls/debug/deps/hello-2ce71755fe43920d.rmeta: src/main.rs
+
+/home/tamas/repos/tmux/hello/target/rls/debug/deps/hello-2ce71755fe43920d.d: src/main.rs
+
+src/main.rs:
